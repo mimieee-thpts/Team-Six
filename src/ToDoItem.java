eecd9f2e0a4e5e0ad3cec80a0a58672ff986731b
Victor <vxrto1901@gmail.com>
2026-03-02 16:45:27 +1000

    completing list-operations

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class ToDoItem {
+    private String description;
+    private boolean isDone;
+
+    // TODO Now:
+    // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
+    public ToDoItem() {
+        string = "Feed the cats.";
+        isDone = false;
+    }
+    // Add getters and setters for each field
+    public string getString() {
+        return this.String;
+    }
+
+    private void setString(string value) {
+        this.String = value;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+
+    }
+
+    private void setIsDone(boolean value) {
+        this.isDone = value;
+    }
 }
