d230878a80bf1ca9ee51a38ece826a81a7252040
i-lac <241275512+i-lac@users.noreply.github.com>
2026-03-03 17:56:01 +1000

    check off authServ constructor task

--- src/AuthenticationService.java
+++ src/AuthenticationService.java
@@ -17,7 +17,7 @@ public class AuthenticationService implements IAuthenticationService {
         return null;
     }
 
-    // TODO Now: Add a constructor to initialize the users list with the default user
+    // DONE TODO Now: Add a constructor to initialize the users list with the default user
 
     // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
 
